f487bd1afdd465e79f8a20ab3172614db87c2844
Gail Rosen <gailr@picotte001.cm.cluster>
2024-01-22 12:41:22 -0500

    Winter 2024 edits

--- edirect_container_run_script/slurm_edirect_test.sh
+++ edirect_container_run_script/slurm_edirect_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 ### !!! CHANGE !!! the email address to your drexel email
-#SBATCH --mail-user=glr26@drexel.edu
+#SBATCH --mail-user=youremail@drexel.edu
 ### !!! CHANGE !!! the account - you need to consult with the professor
-#SBATCH --account=rosenclassPrj
+#SBATCH --account=eces450650prj
 ### select number of nodes (usually you need only 1 node)
 #SBATCH --nodes=1
 ### select number of tasks per node
@@ -15,9 +15,9 @@
 ### memory size required per node (this is important, you also need to estimate a upper bound)
 #SBATCH --mem=1GB
 ### select the partition "def" (this is the default partition but you can change according to your application)
-#SBATCH --partition=def
+#SBATCH --partition=edu
 
 # run this file by executing the name and piping out to file, e.g.: ./get_16S_from_pyogenes.sh > rRNAs_v2.fna
 
 #Assign the Accession ID of the genome to a variable
-singularity run --bind /ifs/groups/eces450650Grp/test_slurm/bio-course-materials/edirect_container_run_script:/run_script /ifs/groups/eces450650Grp/containers/edirect_latest.sif bash /run_script/rng_example.sh > rRNAs_v2.fna
+singularity run --fakeroot --bind /ifs/groups/eces450650Grp/test_slurm/bio-course-materials/edirect_container_run_script:/run_script /ifs/groups/eces450650Grp/containers/edirect_latest.sif bash /run_script/rng_example.sh > rRNAs_v2.fna
